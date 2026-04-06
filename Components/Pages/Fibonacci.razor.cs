9075cca57a6e4b5bf78f901afde839165eb3666a
jhgudleik <jhgudleik@gmail.com>
2026-04-06 06:45:25 +0300

    homework power fibonacci done

---
+++ Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,23 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+        private int count = 1;
+        private List<long> fibonacciSequence = new();
+
+        private void GenerateFibonacci()
+        {
+            fibonacciSequence.Clear();
+
+            long a = 0, b = 1;
+            for (int i = 0; i < count; i++)
+            {
+                fibonacciSequence.Add(a);
+                (a, b) = (b, a + b);
+            }
+        }
+
+        protected override void OnInitialized() => GenerateFibonacci();
+
+    }
+}
---
+++ Components/Pages/Power.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        private double baseValue = 2;
+        private int exponent = 0;
+        private double result = 1;
+
+        private void CalculatePower()
+        {
+            result = Math.Pow(baseValue, exponent);
+        }
+
+    }
+}
