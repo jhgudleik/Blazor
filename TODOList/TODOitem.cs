896766bc7f1092206da788a3a800462acf5266a3
jhgudleik <jhgudleik@gmail.com>
2026-04-12 11:24:02 +0300

    Projects at home

---
+++ TODOList/TODOitem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODOList
+{
+    public class TODOitem
+    {
+        public string Title { get; set; }
+        public bool DONE { get; set; }
+
+    }
+}
