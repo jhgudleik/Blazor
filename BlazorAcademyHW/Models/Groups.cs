a19f39bbd869fabfce3250eee64c5570ea0acb82
jhgudleik <jhgudleik@gmail.com>
2026-04-27 05:42:31 +0300

    groups filtered done

--- BlazorAcademyHW/Models/Groups.cs
+++ BlazorAcademyHW/Models/Groups.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorAcademyHW.Models;
 
 public class Groups
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int group_id { get; set; }
     public string? group_name { get; set; }
     public int direction { get; set; }
